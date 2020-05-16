e0a0d7b9862dec0903e5f3938ab8be84d43a64fa
MarcoCilona <marco.cilona@gmail.com>
2020-05-16 23:46:38 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
   
 #!/usr/bin/env sh
 
+## Set git users to the commit we are building from
+git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
+
 # abort on errors
 set -e
 

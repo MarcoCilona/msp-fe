f2f5926f87cef90a0a3c20e67000fd55b2b6eccf
MarcoCilona <marco.cilona@gmail.com>
2020-08-25 22:28:00 +0200

    Added i18n

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 import App from './App.vue';
 import router from './router/index';
 import './quasar';
@@ -7,7 +8,42 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  fallbackLocale: 'en',
+  locale: navigator.language,
+  silentTranslationWarn: true,
+});
+
+const setLocaleMessages = async () => {
+  const language = (navigator.language && navigator.language.split('-')[0]) || '';
+  try {
+    const messages = await import(
+      /* webpackChunkName: "lang-[request]" */ `@/localization/${language}.json`
+    );
+    console.log('language', messages);
+    // If the language selected is not english, import english labels as fallbacks
+    if (language !== 'it') {
+      const en = await import(
+        /* webpackChunkName: "lang-[request]" */ '@/localization/en.json'
+      );
+      i18n.setLocaleMessage('en', en.en);
+    }
+
+    i18n.setLocaleMessage(language, messages);
+  } catch (e) {
+    const en = await import(
+      /* webpackChunkName: "lang-[request]" */ '@/localization/en.json'
+    );
+    i18n.setLocaleMessage('en', en);
+  }
+};
+
+setLocaleMessages();
+
 new Vue({
+  i18n,
   render: (h) => h(App),
   router,
 }).$mount('#app');

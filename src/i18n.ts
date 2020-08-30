1d259bb5de2970fb3a94dd6a09705bae3655da29
MarcoCilona <marco.cilona@gmail.com>
2020-08-30 16:10:47 +0200

    Adde dynamic language set.

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,8 @@ import VueI18n, { LocaleMessages } from 'vue-i18n';
 
 Vue.use(VueI18n);
 
+const acceptedLocales = ['en', 'it'];
+
 function loadLocaleMessages(): LocaleMessages {
   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
   const messages: LocaleMessages = {};
@@ -16,8 +18,16 @@ function loadLocaleMessages(): LocaleMessages {
   return messages;
 }
 
+function isOneOfAcceptedLanguage(): string {
+  const browserLanguage = navigator.language.split('-')[0];
+
+  if (acceptedLocales.indexOf(browserLanguage.toLowerCase()) !== -1) return browserLanguage;
+
+  return process.env.VUE_APP_I18N_FALLBACK_LOCALE as string;
+}
+
 export default new VueI18n({
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+  locale: isOneOfAcceptedLanguage(),
   messages: loadLocaleMessages()
 });

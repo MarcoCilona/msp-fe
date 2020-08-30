adfeb3020711471734b16c81d3a33afdeeee05b5
MarcoCilona <marco.cilona@gmail.com>
2020-08-30 16:02:27 +0200

    Added language toggle

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -26,6 +26,8 @@ q-toolbar.text-white.position-fixed.shadow-2.navbar
         q-list(style="min-width: 100px")
           q-item.product-category(v-for='(product, index) in products' :key='index')
             q-item-section(@click='$router.push({ path: `/product/${product}` })') {{ product }}
+  .cursor-pointer(@click='handleLanguageSwitch')
+    | {{ $i18n.locale.toUpperCase() }}
 </template>
 
 <script lang="ts">
@@ -33,14 +35,16 @@ import { Component, Vue } from 'vue-property-decorator';
 
 import { mainProducts } from '@/dictionary/index';
 
+const EN = 'en';
+const IT = 'it';
+
 @Component
 export default class Navbar extends Vue {
   model: string = '';
   products: string[] = mainProducts;
-  options: any[] = [];
 
-  created() {
-    this.options = [
+  get options() {
+    return [
       {
         label: this.$t('NAV_ITEM.HOME'),
         path: '',
@@ -68,6 +72,25 @@ export default class Navbar extends Vue {
       }
     ];
   }
+
+  handleLanguageSwitch() {
+    let language: string = '';
+
+    switch (this.$i18n.locale) {
+      case IT:
+        language = EN;
+        break;
+      case EN:
+        language = IT;
+        break;
+      default:
+        language = process.env.VUE_APP_I18N_FALLBACK_LOCALE as string;
+    }
+
+    this.$i18n.locale = language;
+  }
+
+  // Getters and setters
 }
 </script>
 

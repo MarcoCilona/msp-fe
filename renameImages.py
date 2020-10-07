767f47bcf84957a876a02acdc23bdf327a613c99
MarcoCilona <marco.cilona@gmail.com>
2020-10-08 00:36:33 +0200

    Improved python script for image renaming

--- renameImages.py
+++ renameImages.py
@@ -5,6 +5,7 @@ folder = sys.argv[1]
 f_list = os.listdir(f"./src/assets/products/{folder}")
 
 for index, file in enumerate(f_list):
-    os.rename(f"./src/assets/products/{folder}/{file}", f"./src/assets/products/{folder}/img_{index}.jpg")
+    if file.find('img_', 0, 4) == -1:
+        os.rename(f"./src/assets/products/{folder}/{file}", f"./src/assets/products/{folder}/img_{index}.jpg")
 
 print(folder, os.listdir(f"./src/assets/products/{folder}"))
